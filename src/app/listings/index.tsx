4ae3bf3a62792cf3ea0078cd8a389a54ab255a43
ttabong <ssangttabong@kakao.com>
2025-06-22 02:27:17 +0900

    배포용 커밋 메시지 작성

--- src/app/listings/index.tsx
+++ src/app/listings/index.tsx
@@ -1,4 +1,3 @@
-// src/pages/listings/index.tsx
 import ListingsPage from '@/components/Listings/ListingsPage';
 import supabase from '@/lib/supabaseClient';
 
@@ -8,5 +7,11 @@ export default async function AllListingsPage() {
     .select('*')
     .order('created_at', { ascending: false });
 
-  return <ListingsPage initialListings={listings ?? []} />;
+  return (
+    <ListingsPage
+      title="전체 매물 리스트"
+      description="수 부동산이 보유한 전체 매물입니다."
+      initialListings={listings ?? []}
+    />
+  );
 }
